6a7b238fd0405e481f544ea05c1fbfd8a9a0d404
Akshay Murthy <akshaymurthy@outlook.com>
2024-05-31 04:02:12 -0500

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+    state = { fact: '' };
+    componentDidMount(){
+        this.setState({fact:"Click the button below to get a cool fact about a number!"});
+    }
+    fetchFact = () =>{
+        const number = document.getElementById('input').value;
+        if (number && Number.isInteger(Number(number))&&number>=0){
+        axios.get('http://numbersapi.com/' + number)
+        .then((response)=>{
+            const fact = response.data;
+            console.log(fact);
+            this.setState({fact:fact})
+        }).catch((error)=>{
+            console.log(error);
+        })
+        }
+        else{
+            this.setState({fact:"Please enter a positive integer"})
+        }
 }
-
+    
+    render(){
+        const {fact} = this.state;
+        return(
+            <div className='app'>
+                <div className='card'>
+                    <h1 className='heading'>{fact}</h1>
+                    <input id = 'input' className='number-input' type = 'number' placeholder='Type A Number Here'></input>
+                    <button className='button' onClick={this.fetchFact}>
+                        <span>Click Here!</span>
+                    </button>
+                </div>
+            </div>
+        )
+    }
+}
 export default App;
